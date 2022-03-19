7253c15daace6ae2b3f249c9e51af65f7e9ba5b8
Nguyen Thuan <thuan.n@cs.ventures>
2022-03-20 01:50:24 +0700

    Add Annotate gem

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :bigint           not null, primary key
+#  user_id     :bigint           not null
+#  url         :string           not null
+#  title       :string           not null
+#  description :text             not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Movie < ApplicationRecord
   acts_as_votable
 
