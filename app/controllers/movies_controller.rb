be31febae3d0cc2d655f5b85dea766ea729d2e19
Nguyen Thuan <thuan.n@cs.ventures>
2022-03-20 01:22:00 +0700

    Owner is able to delete themself

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class MoviesController < ApplicationController
+  before_action :set_movie, only: :destroy
+
   def create
-    @movie = Movie.create(movie_params)
+    @movie = Movie.create!(movie_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @movie.destroy!
     redirect_to root_path
   end
 
@@ -10,5 +17,9 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(:title, :url, :description, :user_id)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
   end
 end
